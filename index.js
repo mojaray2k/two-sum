efe787e15026fea193e73f7832d3f20e5692e1f2
Amen Moja Ra <mojaray2k@gmail.com>
2021-07-11 22:54:16 +0000

    Implemented two sum

--- index.js
+++ index.js
@@ -1,4 +1,22 @@
 function twoSum(numArray, sum) {
   // returns every pair of numbers from 'numArray'
   // that add up to the 'sum'
+  var pairs = [];
+  var hashTable = [];
+
+  for(var i = 0; i < numArray.length; i++) {
+    var currNum = numArray[i];
+    var counterpart = sum - currNum;
+    if(hashTable.indexOf(counterpart) !== -1){
+      pairs.push([currNum, counterpart]);
+    }
+    hashTable.push(currNum)
+  }
+  return pairs;
 }
+
+// twoSum([1,6,4,5,3,3], 7);
+// => [ [ 6, 1 ], [ 3, 4 ], [ 3, 4 ] ]
+
+twoSum([40,11, 19, 17, -12], 28);
+// => [ [ 17, 11 ], [ -12, 40 ] ]
